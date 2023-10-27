6053964d1d3533318cf9404d063aad7bf3fd9bb6
Mohamed Amine Mtar <mohamedamine.mtar@esprit.tn>
2023-10-27 16:59:34 +0100

    hh

--- src/test/java/tn/esprit/devops_project/RepositoryTest/InvoiceRepositoryTests.java
+++ src/test/java/tn/esprit/devops_project/RepositoryTest/InvoiceRepositoryTests.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class InvoiceRepositoryTests {
 
     @Autowired
